c66aae97c82fc674e4d360862525f9e5b44e6810
dortmundia <csulienjatek@gmail.com>
2021-02-19 16:28:48 +0100

    müködés

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String FILE_NAME = "scanned.csv";
+    private Button buttonOut, buttonScan;
+    private TextView textView;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.getDefault());
+        String timeNow = sdf.format(new Date());
+
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intentIntegrator.setPrompt("QR Code Szkennelés");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+
+        buttonOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String text = textView.getText().toString() +", " + timeNow ;
+                FileOutputStream fos=null;
+
+
+                try {
+                    fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                    fos.write(text.getBytes());
+
+                    String mentes = "Mentés helye: " + getFilesDir() + "/" + FILE_NAME;
+                   // Toast.makeText(this, mentes, Toast.LENGTH_LONG).show(); valami nem tetszik neki :S
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (fos != null) {
+                        try {
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            Toast.makeText(this, "Kiléptél a Scanből", Toast.LENGTH_SHORT).show();
+        } else {
+            textView.setText("QR Code tartalma: " + result.getContents());
+
+            /*
+            Uri uri = Uri.parse(result.getContents());
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            */
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void init() {
+        buttonOut = findViewById(R.id.button_out);
+        buttonScan = findViewById(R.id.button_scan);
+        textView = findViewById(R.id.textView);
+
     }
 }
